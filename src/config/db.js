2889d41db728fd2fcd7f81138948fccf9910da82
Cássia <cascrisbern@gmail.com>
2022-01-26 16:01:45 -0300

    db config changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg')
 
 module.exports = new Pool ({
-  user: "foodfyuser",
-  password: "",
-  host: "localhost",
+  user: process.env.DB_USER || "foodifyuser",
+  password: process.env.DB_PASSWORD || "foodifymypass",
+  host: process.env.DB_HOST || "localhost",
   port: 5432,
-  database: "launchbase_foodfy"
+  database: process.env.DB_DATABASE || "foodify"
 })
